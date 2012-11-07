b6ccffb2bb717d0a48ea1d720ecaa74af58dbd37
airtel <info@airtel.lv>
2012-11-07 09:36:44 +0200

    Added custom error page

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -1,62 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-<title>404 Page Not Found</title>
-<style type="text/css">
+<html lang="en" class="error_page">
+    <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Error Page - 404</title>
+        <!-- Bootstrap framework -->
+        <link rel="stylesheet" href="<?php echo config_item('base_url');?>assets/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="<?php echo config_item('base_url');?>assets/bootstrap/css/bootstrap-responsive.min.css" />
+        <!-- main styles -->
+        <link rel="stylesheet" href="<?php echo config_item('base_url');?>assets/css/error.css" />
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Jockey+One" />
+    </head>
+    <body>
 
-::selection{ background-color: #E13300; color: white; }
-::moz-selection{ background-color: #E13300; color: white; }
-::webkit-selection{ background-color: #E13300; color: white; }
+        <div class="error_box">
+            <h1><?php echo $heading; ?></h1>
+            <p><?php echo $message; ?></p>
+            <a href="javascript:history.back()" class="back_link btn btn-small">Go back</a>
+        </div>
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
-
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	-webkit-box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
-</head>
-<body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
-</body>
+    </body>
 </html>
